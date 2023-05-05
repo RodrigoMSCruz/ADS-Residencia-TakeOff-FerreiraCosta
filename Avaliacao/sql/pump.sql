22c662faa3f199f16b6b137aa6fd666a39496739
RodrigoMSCruz <rodrigomoraessantacruz@yahoo.com.br>
2023-05-04 21:41:05 -0300

    Documentação e primeiros scripts SQL para o banco.

---
+++ Avaliacao/sql/pump.sql
@@ -0,0 +1,18 @@
+INSERT INTO PRODUTO (Codigo, Descricao, Preco)
+VALUES (100, "Faqueiro Tramontina", 359.60);
+
+INSERT INTO PRODUTO (Codigo, Descricao, Preco)
+VALUES (200, "Torneira 3/4 Tigre", 30.9);
+
+INSERT INTO PRODUTO (Codigo, Descricao, Preco)
+VALUES (220, "Geladeira", 2000);
+
+INSERT INTO PRODUTO (Codigo, Descricao, Preco)
+VALUES (360, "Cadeira Tramontina", 120.99);
+
+INSERT INTO PRODUTO (Codigo, Descricao, Preco)
+VALUES (400, "Chaleira", 200);
+
+INSERT INTO PRODUTO (Codigo, Descricao, Preco)
+VALUES (500, "TV Samsung 52", 1899);
+
